9ca64ff6d89b3c46ed195b2ece151c619d4985d4
flipflop133 <fbechet45@gmail.com>
2020-05-23 16:14:40 +0200

    bring some improvements to the script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 ###################################################################
 #  _____   ____ _______   ____          _____ _  ___    _ _____   #
 # |  __ \ / __ \__   __| |  _ \   /\   / ____| |/ / |  | |  __ \  #
@@ -14,39 +13,44 @@ DefaultIFS=$IFS
 
 # Display title
 clear
-echo "DOT BACKUP"|lolcat || echo -e echo -e "\e[96mDOT BACKUP\e[0m"
-
+echo -e "\e[96mDOT BACKUP SCRIPT\e[0m"
+echo ''
 # define backup directory
 echo -e "\e[96mBackup directory:\e[0m"
 pwd=$(pwd)
-echo "$pwd"
+echo "$pwd/dotfiles"
 dot=$pwd
 
 # dot files directories
-directories=(
-    $HOME/.zshrc
-    $HOME/.config/alacritty
-    "$HOME/.config/Code - OSS/User/settings.json"
-    $HOME/.config/conky
-    $HOME/.config/dunst
-    $HOME/.config/fontconfig
-    $HOME/.config/i3  
-    $HOME/.config/picom
-    $HOME/.config/polybar
-    $HOME/.config/pythons-scripts
-    $HOME/.config/rofi
-)
+directories=()
+input="dotfiles_list.txt"
+while IFS= read -r line
+do
+    IFS=''
+    directories+=($HOME/$line)
+    IFS= read -r line
+done < "$input"
 
 # clean old dotfiles
 cleaner(){
+    echo ''
     read -r -p "Clean old dotfiles(those in the cloned repo)? [y/N]" response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-    then
-        echo -e "\e[96mOld dotfiles will be deleted.\e[0m" 
-        clean="true"
+    then  
+        echo -e "\e[96mThe following directory will be deleted:\e[0m"
+        echo "$dot/dotfiles"
+        read -r -p "Do you want to proceed? [y/N]" response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then 
+            clean="true"
+            echo -e "\e[96mOld dotfiles will be deleted.\e[0m"
+        else 
+            echo -e "\e[96mSkipping cleaning.\e[0m"
+        fi
     else
         echo -e "\e[96mSkipping cleaning.\e[0m" 
     fi
+    echo ''
 }
 
 # backup files
@@ -61,34 +65,46 @@ backup(){
     fi
     # backup new dotfiles
     mkdir $dot/dotfiles
+
     for dir in ${directories[@]}; do
         echo "$dir"
         # copy the dotfile
         cp -R -f "$dir" $dot/dotfiles
     done
+    echo -e "\e[96mDotfiles backup done!\e[0m"
     # Reset IFS to its default value
     IFS=$DefaultIFS
+    echo ''
 }
 
 # backup list of installed packages
 backup_app(){
     read -r -p "Backup explicitly installed app list? [y/N]" response
     if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-    then
-        echo -e "\e[96mDoing app list backup.\e[0m" 
-        pacman -Qe > appList.txt
+    then    
+        pacman -Qe > appList.txt && echo -e "\e[96mApp list backup done!\e[0m"
     else
-        echo -e "\e[96mSkipping app list backup.\e[0m" 
+        echo -e "\e[96mSkipping app list backup.\e[0m"
     fi
+    echo ''
 }
 
 
-# push files to github
-GIT=`which git`
+# push files to the user repo
+user_repo=$(git config --get remote.origin.url)
 git(){
-    ${GIT} add .
-    ${GIT} commit -m "update dotfiles"
-    ${GIT} push
+    echo -e "\e[96mDo you want to push the backup to the following ${GIT} repo?\e[0m"
+    echo "$user_repo"
+    read -r -p "[y/N]" response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo -e "\e[96mPushing files to ${GIT}.\e[0m"
+        GIT=`which git`
+        ${GIT} add dotfiles appList.txt
+        ${GIT} commit -m "update dotfiles"
+        ${GIT} push
+    fi
+    echo ''
 }
 
 # execute the functions
