59ef2c369d8ebb3ac92b04b718e61f848accdffb
flipflop133 <fbechet45@gmail.com>
2020-05-24 11:14:40 +0200

    add restore function

--- backup.sh
+++ backup.sh
@@ -1,104 +1,145 @@
 #!/bin/bash
-###################################################################
-#  _____   ____ _______   ____          _____ _  ___    _ _____   #
-# |  __ \ / __ \__   __| |  _ \   /\   / ____| |/ / |  | |  __ \  #
-# | |  | | |  | | | |    | |_) | /  \ | |    | ' /| |  | | |__) | #
-# | |  | | |  | | | |    |  _ < / /\ \| |    |  < | |  | |  ___/  #
-# | |__| | |__| | | |    | |_) / ____ \ |____| . \| |__| | |      #
-# |_____/ \____/  |_|    |____/_/    \_\_____|_|\_\\____/|_|      #
-###################################################################
+###################################################################################################################
+#                                                                                                                 #
+#  _____   ____ _______ ______ _____ _      ______  _____   __  __          _   _          _____ ______ _____     #
+# |  __ \ / __ \__   __|  ____|_   _| |    |  ____|/ ____| |  \/  |   /\   | \ | |   /\   / ____|  ____|  __ \    #
+# | |  | | |  | | | |  | |__    | | | |    | |__  | (___   | \  / |  /  \  |  \| |  /  \ | |  __| |__  | |__) |   #
+# | |  | | |  | | | |  |  __|   | | | |    |  __|  \___ \  | |\/| | / /\ \ | . ` | / /\ \| | |_ |  __| |  _  /    #
+# | |__| | |__| | | |  | |     _| |_| |____| |____ ____) | | |  | |/ ____ \| |\  |/ ____ \ |__| | |____| | \ \    #
+# |_____/ \____/  |_|  |_|    |_____|______|______|_____/  |_|  |_/_/    \_\_| \_/_/    \_\_____|______|_|  \_\   #
+#                                                                                                                 #                                                                                                        
+###################################################################################################################
 
 # Save default IFS value
 DefaultIFS=$IFS
 
-# Display title
-clear
-echo -e "\e[96mDOT BACKUP SCRIPT\e[0m"
-echo ''
-# define backup directory
-echo -e "\e[96mBackup directory:\e[0m"
-dot=$(pwd)
-echo "$dot"
+# Backup utility
+backup_utility(){
+    # Display title
+    clear
+    echo -e "\e[96mDOTFILES BACKUP SCRIPT\e[0m"
+    echo ''
+    # define backup directory
+    echo -e "\e[96mBackup directory:\e[0m"
+    dot=$(pwd)
+    echo "$dot"
 
-# dot files directories
-directories=()
-input="dotfiles_list.txt"
-while IFS= read -r line
-do
-    IFS=''
-    directories+=($line)
-    IFS=$DefaultIFS
-done < "$input"
+    # dot files directories
+    directories=()
+    input="dotfiles_list.txt"
+    while IFS= read -r line
+    do
+        IFS=''
+        directories+=($line)
+        IFS=$DefaultIFS
+    done < "$input"
 
-# backup files
-backup(){
-    # Set IFS to '' so spaces are not ignored
-	IFS=''
-    # copy dot files to backup directory
-    echo -e "\e[96mBackuping the following dotfiles:\e[0m"
-    # backup new dotfiles
-    for dir in ${directories[@]}; do
-        echo "$dir"
-        if [[ $dir == *".config"* ]];
-        then
-            temp_dir=${dir#.*g}
-            #mkdir -p "$dot$temp_dir"
-            # copy the dotfile
-            if cp -R -f "$HOME/$dir" $dot$temp_dir ;
-            then :
-            else 
-                mkdir -p "$dot$temp_dir"
-                cp -R -f "$HOME/$dir" $dot$temp_dir
-            fi
-            echo "$HOME/$dir$dot$temp_dir"
-        else
-            mkdir -p "$dot$dir"
-            echo "$dot/$dir"
+    # backup files
+    backup(){
+        # Set IFS to '' so spaces are not ignored
+        IFS=''
+        # copy dotfiles to backup directory
+        echo -e "\e[96mBackuping the following dotfiles:\e[0m"
+        # backup new dotfiles
+        for dir in ${directories[@]}; do
+            echo "$dir"
+            mkdir -p "$dot/$dir"
             # copy the dotfile
             cp -R -f "$HOME/$dir" $dot/$dir
-            echo "$HOME/$dir"
+        done
+        echo -e "\e[96mDotfiles backup done!\e[0m"
+        # Reset IFS to its default value
+        IFS=$DefaultIFS
+        echo ''
+    }
+
+    # backup list of installed packages
+    backup_app(){
+        read -r -p "Backup explicitly installed app list? [y/N]" response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then    
+            pacman -Qe > appList.txt && echo -e "\e[96mApp list backup done!\e[0m"
+        else
+            echo -e "\e[96mSkipping app list backup.\e[0m"
         fi
-    done
-    echo -e "\e[96mDotfiles backup done!\e[0m"
-    # Reset IFS to its default value
-    IFS=$DefaultIFS
-    echo ''
+        echo ''
+    }
+
+
+    # push files to the user repo
+    user_repo=$(git config --get remote.origin.url)
+    git(){
+        GIT=`which git`
+        echo -e "\e[96mDo you want to push the backup to the following repo?\e[0m"
+        echo "$user_repo"
+        read -r -p "[y/N]" response
+        if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+            echo -e "\e[96mPushing files to your repo.\e[0m"  
+            ${GIT} add .
+            ${GIT} reset backup.sh
+            read -r -p "Commit message:" response
+            ${GIT} commit -m "$response"
+            ${GIT} push
+        fi
+        echo ''
+    }
+
+    # execute the functions
+    backup
+    backup_app
+    git
+    echo -e "\e[96mBackup completed!\e[0m"
 }
 
-# backup list of installed packages
-backup_app(){
-    read -r -p "Backup explicitly installed app list? [y/N]" response
-    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-    then    
-        pacman -Qe > appList.txt && echo -e "\e[96mApp list backup done!\e[0m"
-    else
-        echo -e "\e[96mSkipping app list backup.\e[0m"
-    fi
-    echo ''
+# Restore utility
+restore_utility(){
+    clear
+
+    # dot files directories
+    directories=()
+    input="dotfiles_list.txt"
+    while IFS= read -r line
+    do
+        IFS=''
+        directories+=($line)
+        IFS=$DefaultIFS
+    done < "$input"
+
+    # restore files
+    restore(){
+        # Set IFS to '' so spaces are not ignored
+        IFS=''
+        # copy dotfiles to their directories
+        echo -e "\e[96mRestoring the following dotfiles:\e[0m"
+        # restore dotfiles
+        for dir in ${directories[@]}; do
+            echo "$HOME/$dir"
+            mkdir -p "$HOME/$dir"
+            # copy the dotfile
+            echo "$dot/$dir$HOME/$dir"
+            cp -R -f $dot/$dir "$HOME/$dir"
+        done
+        echo -e "\e[96mDotfiles restore done!\e[0m"
+        # Reset IFS to its default value
+        IFS=$DefaultIFS
+        echo ''
+    }
+
+    # execute the functions
+    restore
 }
 
-
-# push files to the user repo
-user_repo=$(git config --get remote.origin.url)
-git(){
-    GIT=`which git`
-    echo -e "\e[96mDo you want to push the backup to the following repo?\e[0m"
-    echo "$user_repo"
-    read -r -p "[y/N]" response
-    if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
-    then
-        echo -e "\e[96mPushing files to your repo.\e[0m"  
-        ${GIT} add .
-        ${GIT} reset backup.sh
-        read -r -p "Commit message:" response
-        ${GIT} commit -m "$response"
-        ${GIT} push
-    fi
-    echo ''
-}
-
-# execute the functions
-backup
-backup_app
-git
-echo -e "\e[96mBackup completed!\e[0m"
+# MAIN
+clear
+echo -e "\e[96mDOTFILES MANAGER SCRIPT\e[0m"
+echo ''
+echo -e "\e[96m1)Backup utility\e[0m"
+echo -e "\e[96m2)Restore utility\e[0m"
+read -r -p "Enter a selection:" response
+if [ "$response" == "1" ]
+then
+    backup_utility
+else
+    restore_utility
+fi
